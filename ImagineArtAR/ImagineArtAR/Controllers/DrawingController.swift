430912df04af105052415ca9171a9d66e4a8e0c4
Uchenna Aguocha <uk.aguocha@gmail.com>
2018-04-22 22:43:15 -0700

    Added relative positioning

---
+++ ImagineArtAR/ImagineArtAR/Controllers/DrawingController.swift
@@ -0,0 +1,157 @@
+//
+//  DrawingController.swift
+//  ImagineArtAR
+//
+//  Created by Uchenna  Aguocha on 4/21/18.
+//  Copyright © 2018 Uchenna  Aguocha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SceneKit
+import ARKit
+
+protocol DrawingPaletteDelegate {
+    
+}
+
+class DrawingController: UIViewController {
+    
+    // MARK: UIComponents
+    var sceneView: ARSCNView = {
+        let sv = ARSCNView()
+        sv.backgroundColor = UIColor.blue
+        return sv
+    }()
+    
+    let addButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Add", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor.red
+        button.addTarget(self, action: #selector(handleAdd), for: .touchDragInside)
+        return button
+    }()
+    
+    let resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Reset", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor.blue
+        button.addTarget(self, action: #selector(handleResetSession), for: .touchDragInside)
+        return button
+    }()
+    
+    // Track the position of your device relative to world around it
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSceneView()
+    }
+    
+    // MARK: Methods
+    
+    func setupSceneView() {
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        sceneView.session.run(configuration)
+        sceneView.autoenablesDefaultLighting = true
+        
+        setupAutoLayout()
+    }
+    
+    func setupAutoLayout() {
+        
+        view.addSubview(sceneView)
+        sceneView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        view.addSubview(addButton)
+        addButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 20, paddingBottom: 20, paddingRight: 0, width: 80, height: 80)
+        
+        view.addSubview(resetButton)
+        resetButton.anchor(top: nil, left: nil, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 20, paddingRight: 20, width: 80, height: 80)
+    }
+    
+    func randomNumber(firstNumber: CGFloat, secondNumber: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNumber - secondNumber) + min(firstNumber, secondNumber)
+    }
+    
+    // MARK: ObjC Methods
+    
+    @objc func handleAdd() {
+        let node = SCNNode()
+//        let path = UIBezierPath()
+//        path.move(to: CGPoint(x: 0, y: 0))
+//        path.addLine(to: CGPoint(x: 0, y: 0.2))
+//        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+//        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+//        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+//        node.geometry = shape
+//        node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+//        node.geometry = SCNSphere(radius: 0.2)
+//        node.geometry = SCNCylinder(radius: 0.2, height: 0.2)
+//        node.geometry = SCNCone(topRadius: 0.3, bottomRadius: 0.3, height: 0.4)
+//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+//        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0.2,0.3,-0.2)
+        
+        let cylinderNode  = SCNNode()
+        cylinderNode.geometry = SCNCylinder(radius: 0.1, height: 0.3)
+        cylinderNode.geometry?.firstMaterial?.specular.contents = UIColor.red
+        cylinderNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        cylinderNode.position = SCNVector3(-0.1,0.5,-0.5)
+        
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        boxNode.geometry?.firstMaterial?.specular.contents = UIColor.white
+        boxNode.position = SCNVector3(0,-0.05,0)
+        
+        let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
+        doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        doorNode.position = SCNVector3(0,-0.02,0.053)
+        
+        node.addChildNode(boxNode)
+        boxNode.addChildNode(doorNode)
+        sceneView.scene.rootNode.addChildNode(node)
+        //sceneView.scene.rootNode.addChildNode(cylinderNode)
+    }
+    
+    @objc func handleResetSession() {
+        /* Remove the existing node from the root node
+         , reset tracking, & remove anchors
+         */
+        sceneView.session.pause()
+        sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ImagineArtAR/ImagineArtAR/Extensions/UIView+Extension.swift
@@ -0,0 +1,46 @@
+//
+//  UIView+Extension.swift
+//  ImagineArtAR
+//
+//  Created by Uchenna  Aguocha on 4/21/18.
+//  Copyright © 2018 Uchenna  Aguocha. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func anchorSize(to view: UIView) {
+        widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat,  paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
